76d5497d567e9a6dc49d9ab73c67bf636e188a1a
carmenromo <romoluque_c@pn2294397.lanl.gov>
2023-07-06 17:02:07 -0600

    Add uproot exception

--- get_peak_height_run_all_files.py
+++ get_peak_height_run_all_files.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import subprocess
+import uproot
 
 files_dir = sys.argv[1]
 out_path  = sys.argv[2]
@@ -18,3 +19,5 @@ for filename in os.listdir(files_dir):
         subprocess.call(['python', 'get_peak_height.py', files_dir, filename, out_path])
     except FileNotFoundError:
         subprocess.call(['python3', 'get_peak_height.py', files_dir, filename, out_path])
+    except uproot.exceptions.KeyInFileError:
+        continue
