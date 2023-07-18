a67df1170e2e9bb6d214ee357217ca6cd8f999c3
carmenromo <romoluque_c@pn2294397.lanl.gov>
2023-07-18 17:02:57 -0600

    Add useful functions

---
+++ blr_functions.py
@@ -0,0 +1,97 @@
+### BLR functions based adapted from IC
+
+import numpy as np
+
+from scipy import signal as sgn
+
+coeff_c_NEXT   = np.array([0.00000042504, 0.00000055255, 0.00000035999, 0.00000035639, 0.00000047635, 0.00000059184, 0.00000055974, 0.00000057049, 0.00000038041, 0.00000041167, 0.00000037353, 0.00000048496])
+coeff_blr_NEXT = np.array([0.00053015, 0.00052996, 0.00053319, 0.00052624, 0.00052731, 0.00052813, 0.00051798, 0.00052867, 0.00052046, 0.0005271, 0.00052704, 0.00052508])
+
+
+def deconvolve_signal(signal_daq,
+                      coeff_clean            = 2.905447E-06,
+                      coeff_blr              = 1.632411E-03,
+                      thr_trigger            =     5,
+                      accum_discharge_length =  5000):
+    """
+    Function adapted from IC
+    """
+    
+    thr_acum = thr_trigger / coeff_blr
+    len_signal_daq = len(signal_daq)
+
+    # Compute baseline using the me
+    baseline = np.mean(signal_daq)
+
+    # Reverse sign of signal and subtract baseline
+    signal_daq_bs = baseline - signal_daq
+
+    nn = 400  # fixed at 10 mus
+    # Compute noise
+    noise = np.sum(signal_daq_bs[:nn] ** 2) / nn
+    noise_rms = np.sqrt(noise)
+
+    # trigger line
+    trigger_line = thr_trigger * noise_rms
+
+    b_cf, a_cf    = sgn.butter(1, coeff_clean, 'high', analog=False);
+    signal_daq_bs = sgn.lfilter(b_cf, a_cf, signal_daq_bs)
+
+    signal_r = np.empty(len_signal_daq)
+    acum     = np.zeros(len_signal_daq, dtype=np.double)
+    
+    # Always update signal and accumulator
+    signal_r[0]    = signal_daq_bs[0]
+    coeff_blr_half = coeff_blr / 2
+    
+    trigger_mask   = (signal_daq_bs[:-1] < trigger_line) & (acum[:-1] < thr_acum)
+    acum_discharge = np.where(trigger_mask, np.maximum(1, acum[:-1]) * (1 - coeff_blr), 0)
+    
+    j_values = np.arange(accum_discharge_length)
+    j_values = j_values[j_values < np.sum(trigger_mask)]
+    acum[1:] = np.where(trigger_mask, acum[:-1] + signal_daq_bs[1:], acum[1:])
+    
+    # Discharge the accumulator
+    acum[1:] -= acum_discharge
+    
+    signal_r[1:] = signal_daq_bs[1:] + signal_daq_bs[1:] * coeff_blr_half + coeff_blr * acum[1:]
+
+    return np.asarray(signal_r)
+
+
+def blr_deconv_pmt(pmtrwf,
+                   coeff_c,
+                   coeff_blr,
+                   thr_trigger            =    5,
+                   accum_discharge_length = 5000):
+    """
+    Deconvolve all the PMTs in the event.
+    :param pmtrwf: array of PMTs holding the raw waveform
+    :param coeff_c:     cleaning coefficient
+    :param coeff_blr:   deconvolution coefficient
+    :param n_baseline:  number of samples taken to compute baseline
+    :param thr_trigger: threshold to start the BLR process
+    
+    :returns: an array with deconvoluted PMTs.
+    """
+
+    nwf      = len(pmtrwf)
+    signal_i = pmtrwf.astype(np.double)
+    signal_r = np.zeros(nwf, dtype=np.double)
+
+    signal_r = deconvolve_signal(signal_i,
+                                 coeff_clean            = coeff_c,
+                                 coeff_blr              = coeff_blr,
+                                 thr_trigger            = thr_trigger,
+                                 accum_discharge_length = accum_discharge_length)
+    return signal_r
+
+
+def pmt_deconvolver(rwf):
+    coeff_c    = np.mean(coeff_c_NEXT)
+    coeff_blr  = np.mean(coeff_blr_NEXT)
+
+    return blr_deconv_pmt(rwf,
+                          coeff_c,
+                          coeff_blr)
+    return deconv_pmt
---
+++ fit_functions.py
@@ -0,0 +1,111 @@
+### Fit functions from IC
+
+import numpy as np
+import scipy
+
+from collections import namedtuple
+
+FitFunction = namedtuple('FitFunction', ['fn', 'values', 'errors', 'chi2', 'pvalue', 'cov'])
+
+
+def in_range(data, minval=-np.inf, maxval=np.inf, left_closed=True, right_closed=False):
+    lower_bound = data >= minval if left_closed  else data > minval
+    upper_bound = data <= maxval if right_closed else data < maxval
+    return lower_bound & upper_bound
+
+def get_errors(cov):
+    return np.sqrt(np.diag(cov))
+
+def poisson_sigma(x, default=3):
+    u = x**0.5
+    u[x==0] = default
+    return u
+
+def get_chi2_and_pvalue(ydata, yfit, ndf, sigma=None):
+    if sigma is None:
+        sigma = poisson_sigma(ydata)
+
+    chi2   = np.sum(((ydata - yfit) / sigma)**2)
+    pvalue = scipy.stats.chi2.sf(chi2, ndf)
+
+    return chi2 / ndf, pvalue
+
+def gauss(x, area, mu, sigma):
+    if sigma <= 0.:
+        return np.inf
+    return area/(2*np.pi)**.5/sigma * np.exp(-0.5*(x-mu)**2./sigma**2.)
+
+def fit(func, x, y, seed=(), fit_range=None, **kwargs):
+    if fit_range is not None:
+        sel  = in_range(x, *fit_range)
+        x, y = x[sel], y[sel]
+        if "sigma" in kwargs:
+            kwargs["sigma"] = kwargs["sigma"][sel]
+
+    sigma_r = kwargs.get("sigma", np.ones_like(y))
+    if np.any(sigma_r <= 0):
+        raise ValueError("Zero or negative value found in argument sigma. "
+                         "Errors must be greater than 0.")
+
+    kwargs['absolute_sigma'] = "sigma" in kwargs
+
+    vals, cov = scipy.optimize.curve_fit(func, x, y, seed, **kwargs)
+
+    fitf       = lambda x: func(x, *vals)
+    fitx       = fitf(x)
+    errors     = get_errors(cov)
+    ndof       = len(y) - len(vals)
+    chi2, pval = get_chi2_and_pvalue(y, fitx, ndof, sigma_r)
+
+    return FitFunction(fitf, vals, errors, chi2, pval, cov)
+
+def shift_to_bin_centers(x):
+    return x[:-1] + np.diff(x) * 0.5
+
+
+def f_values(f):
+    _, mu,     sigma     = f.values
+    _, mu_err, sigma_err = f.errors
+    fwhm, fwhm_err = sigma* 2.355, sigma_err* 2.355
+    return mu, mu_err, sigma, sigma_err, fwhm, fwhm_err, f.chi2
+
+def string_fit(f, units='ADCs', ndec=2, print_chi2=True):
+    mu, mu_err, sigma, sigma_err, fwhm, fwhm_err, chi2 = f_values(f)
+    mu_str   = f'$\mu$ = {np.round(mu, ndec)} ± {np.round(mu_err, ndec)} {units}'
+    sig_str  = f'$\sigma$ = {np.round(sigma, ndec)} ± {np.round(sigma_err, ndec)} {units}'
+    chi2_str = f'$\chi^2$ / ndf = {np.round(chi2,  ndec)}'
+    fwhm_str = f'FWHM = {np.round(fwhm, 2)} ± {np.round(fwhm_err, 2)} pes'
+    if print_chi2:
+        return f'{mu_str}'+'\n'+f'{sig_str}'+'\n'+f'{chi2_str}'
+    else:
+        return f'{mu_str}'+'\n'+f'{sig_str}'
+    
+def gaussian_fit_IC(data, bins=100, prange=(-50, 150), ampl=100, mean=0, sigma=10, frange=(-50, 50), title='', ndec=2, print_chi2=True):
+    fig = plt.figure(figsize=(8,5))
+    y, x, _ = plt.hist(data, bins=bins, alpha=0.5, range=prange, histtype='step', linewidth=1.5)
+    f = fit(gauss, shift_to_bin_centers(x), y, (ampl,mean,sigma), fit_range=frange, sigma=np.sqrt(y))
+
+    plt.plot(shift_to_bin_centers(x), gauss(shift_to_bin_centers(x), *f.values[:3]), 'r--',
+             label=string_fit(f, ndec=ndec, print_chi2=print_chi2))
+    plt.errorbar(shift_to_bin_centers(x), y, yerr=np.sqrt(y), fmt='.k', elinewidth=0.5, capsize=2, capthick=1)
+    plt.title(title)
+    plt.xlabel('ADCs')
+    plt.ylabel('Entries/bin')
+    plt.legend()
+    plt.show()
+    
+    return f_values(f)
+
+def gaussian_fit_IC_subplot(ax, data, bins=100, prange=(-50, 150), ampl=100, mean=0, sigma=10, frange=(-50, 50), title='', ndec=2, print_chi2=True):
+    y, x, _ = ax.hist(data, bins=bins, alpha=0.5, range=prange, histtype='step', linewidth=1.5)
+    f = fit(gauss, shift_to_bin_centers(x), y, (ampl,mean,sigma), fit_range=frange, sigma=np.sqrt(y))
+
+    ax.plot(shift_to_bin_centers(x), gauss(shift_to_bin_centers(x), *f.values[:3]), 'r--',
+             label=string_fit(f, ndec=ndec, print_chi2=print_chi2))
+    ax.errorbar(shift_to_bin_centers(x), y, yerr=np.sqrt(y), fmt='.k', elinewidth=0.5, capsize=2, capthick=1)
+    ax.set_title(title)
+    ax.set_xlabel('Amplitude (ADCs)')
+    ax.set_ylabel('Entries/bin')
+    ax.legend(loc='upper right', handlelength=0.5)
+    
+    return f_values(f)
