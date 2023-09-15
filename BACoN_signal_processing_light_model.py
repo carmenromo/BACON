a06c05cd631f01e8b841d5a8a2f3ae3584109367
carmenromo <carmenromoluque@gmail.com>
2023-09-15 16:56:55 -0600

    Add nb

---
+++ BACoN_signal_processing_light_model.py
@@ -0,0 +1,46 @@
+
+import sys
+import uproot
+
+import numpy as np
+
+import peak_functions as pf
+
+arguments = pf.parse_args(sys.argv)
+in_path   = arguments.in_path
+file_name = arguments.file_name
+out_path  = arguments.out_path
+
+if file_name.endswith(".root"):
+    file_name = file_name[:-5]
+filename = f"{in_path}/{file_name}.root"
+infile   = uproot.open(filename)
+RawTree  = infile['RawTree']
+
+outfile = f"{out_path}/BACoN_sig_processing_light_model_{file_name}"
+
+total_SiPMs = 9
+dead_SiPMs  = [3]
+peak_range  = (0, None)
+num_wfs     = None # All wfs to be analyzed
+sipm_thr    = 50 #ADCs
+peak_sep    = 10
+
+int_subt_wfs_filt = [np.array([]) for _ in range(total_SiPMs)]
+
+for channel in range(total_SiPMs):
+    print(channel)
+    if channel in dead_SiPMs:
+        continue
+    try:
+        _, subt_wfs_filt, all_peaks = pf.get_peaks_using_peakutils(RawTree, channel, num_wfs=num_wfs, sipm_thr=sipm_thr, peak_range=peak_range)
+        int_subt_wf_filt = np.sum(subt_wfs_filt, axis=0)
+    except ValueError:
+        continue
+
+    int_subt_wfs_filt[channel] = np.append(int_subt_wfs_filt[channel], int_subt_wf_filt)
+
+int_subt_wfs_filt = np.array(int_subt_wfs_filt, dtype=object)
+
+np.savez(outfile, 
+         int_subt_wfs_filt=int_subt_wfs_filt)
