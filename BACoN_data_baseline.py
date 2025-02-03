53f0be7c76f5712541ee1bb1108dca02855d8577
carmenromo <carmenromoluque@gmail.com>
2025-02-03 13:58:57 -0700

    Add baseline script

---
+++ BACoN_data_baseline.py
@@ -0,0 +1,67 @@
+import sys
+import uproot
+import numpy          as np
+import peak_functions as pf
+
+
+arguments = pf.parse_args(sys.argv)
+in_path   = arguments.in_path
+file_name = arguments.file_name
+out_path  = arguments.out_path
+
+if file_name.endswith(".root"):
+    file_name = file_name[:-5]
+filename = f"{in_path}/{file_name}.root"
+infile   = uproot.open(filename)
+RawTree  = infile['RawTree']
+
+outfile = f"{out_path}/BACoN_data_baseline_{file_name}"
+
+all_chs      = range(13)
+max_smpl_bsl = 650
+
+## Thr values valid from 9/10/2024 since the bias voltage was changed
+std_thr_dict = {0: 13,
+                1: 13,
+                2: 13,
+                3: 13,
+                4: 14,
+                5: 13,
+                6: 12,
+                7: 13,
+                8: 13,
+                9: 30,
+                10: 40,
+                11: 40}
+
+bsl_mean_i_dict = {}
+bsl_mode_i_dict = {}
+bsl_mean_f_dict = {}
+bsl_mode_f_dict = {}
+std_all_dict    = {}
+max_all_dict    = {}
+for ch in all_chs:
+    print(f'Channel {ch}')
+    ## 0) Get waveforms:
+    all_wfs = pf.wfs_from_rawtree(RawTree, ch)
+
+    ## 1) Compute the baseline for each channel (mean and mode at the beginning and at the end of each wf)
+    bsl_mean_i_dict[ch] = pf.compute_baseline_std_lim(all_wfs[:, :max_smpl_bsl],  mode=False, std_lim=3*std_thr_dict[ch])
+    bsl_mode_i_dict[ch] = pf.compute_baseline_std_lim(all_wfs[:, :max_smpl_bsl],  mode=True,  std_lim=3*std_thr_dict[ch])
+    bsl_mean_f_dict[ch] = pf.compute_baseline_std_lim(all_wfs[:, -max_smpl_bsl:], mode=False, std_lim=3*std_thr_dict[ch])
+    bsl_mode_f_dict[ch] = pf.compute_baseline_std_lim(all_wfs[:, -max_smpl_bsl:], mode=True,  std_lim=3*std_thr_dict[ch])
+
+    ## 2) Std of the waveforms
+    std_all_dict[ch] = np.std(all_wfs, axis=1)
+
+    ## 3) Max value of each waveform
+    max_all_dict[ch] = np.max(all_wfs, axis=1)
+
+
+np.savez(outfile,
+         bsl_mean_i_dict=bsl_mean_i_dict,
+         bsl_mode_i_dict=bsl_mode_i_dict,
+         bsl_mean_f_dict=bsl_mean_f_dict,
+         bsl_mode_f_dict=bsl_mode_f_dict,
+         std_all_dict=std_all_dict,
+         max_all_dict=max_all_dict)
